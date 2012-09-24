ab2791c1df49632b43095ad2a478b17b10e22cfc
freexperia <jerpelea@gmail.com>
2012-09-24 19:01:20 +0300

    fuji: remove unneded deps
    those deps are automaticaly built so there is no need to add them in .mk

--- fuji.mk
+++ fuji.mk
@@ -46,11 +46,8 @@ PRODUCT_PACKAGES += \
     gralloc.msm8660 \
     hwcomposer.msm8660 \
     libgenlock \
-    libhwcexternal.so \
-    libhwcservice.so \
     libmemalloc \
     liboverlay \
-    libqdMetaData.so \
     libqdutils \
     libtilerenderer
 
